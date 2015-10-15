47bfb03075a293f6fcdbd1df66c9f1439e45f43c
ChrisBollands <cgbollands@gmail.com>
2015-10-15 18:06:10 +1100

    fixed score counting

--- src/Model/Player.cs
+++ src/Model/Player.cs
@@ -179,18 +179,25 @@ public class Player : IEnumerable<Ship>
 	/// <returns>the result of the attack</returns>
 	internal AttackResult Shoot(int row, int col)
 	{
-		_shots += 1;
 		AttackResult result = default(AttackResult);
-		result = EnemyGrid.HitTile(row, col);
+		result = EnemyGrid.HitTile (row, col);
 
-		switch (result.Value) {
-			case ResultOfAttack.Destroyed:
-			case ResultOfAttack.Hit:
-				_hits += 1;
-				break;
-			case ResultOfAttack.Miss:
-				_misses += 1;
-				break;
+		switch (result.Value)
+		{
+		case ResultOfAttack.Hit:
+			_shots += 1;
+			_hits += 1;
+			break;
+		case ResultOfAttack.Miss:
+			_shots += 1;
+			_misses += 1;
+			break;
+		case ResultOfAttack.Destroyed:
+			_shots += 1;
+			_hits += 1;
+			break;
+		case ResultOfAttack.ShotAlready:
+			break;
 		}
 
 		return result;
